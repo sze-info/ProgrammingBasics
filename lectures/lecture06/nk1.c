da4b7a03aa7503b39c759b01e92cd629b056272b
wajzy <miklos.hatwagner@sze.hu>
2020-10-15 20:29:33 +0200

    Slides up to 20

---
+++ lectures/lecture06/nk1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <iso646.h>
+
+int read(int max) {
+  int number;
+  bool invalid;
+  do {
+    printf("Number: "); scanf("%d", &number);
+    invalid = number<1 or number>max;
+    if(invalid) printf("Hibas adat!\n");
+  } while(invalid);
+  return number;
+}
+
+unsigned long factorial(int n) {
+  if(n < 2) return 1;
+  unsigned long f = 1ul;
+  for(int i=1; i<=n; i++) {
+    f *= i;
+  }
+  return f;
+}
+
+unsigned long combination(int n, int k) {
+  return factorial(n) / (factorial(k)*factorial(n-k));
+}
+
+int main(void) {
+  int n = read(INT_MAX);
+  int k = read(n);
+  printf("%lu\n", combination(n, k));
+  return 0; 
+}
